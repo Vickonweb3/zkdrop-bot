ade120611a31252a0819cb3ce2399f2f2e6a2a05
Vickonweb3 <vnnamdi534@gmail.com>
2025-07-17 15:48:40 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,13 @@
+import os
 
+# 🔐 Secure credentials
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7245698081:AAHvSwVjJ5xsjOXOKRVTtopOterqCGAAMFw")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://zkadmin:nnamdi2010@zksyncbot.xkz56cm.mongodb.net/?retryWrites=true&w=majority&appName=Zksyncbot")
+
+# 👑 Admin
+ADMIN_ID = 7428947778
+OWNER_USERNAME = "@Vickonweb3"
+
+# ⏰ Scheduler
+TASK_INTERVAL_MINUTES = 90  # for random airdrop posts
+SCRAPE_INTERVAL_HOURS = 3   # for scraping potential airdrops
