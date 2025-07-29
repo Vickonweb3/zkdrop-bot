6fbb9635bafe9846043fe961df865b00e78241d5
Vickonweb3 <vnnamdi534@gmail.com>
2025-07-29 15:09:35 +0100

    Create __init__.py

---
+++ tasks/__init__.py
@@ -0,0 +1,9 @@
+# tasks/__init__.py
+
+"""
+Task module for scheduled jobs like automatic airdrop scanning and broadcasting.
+
+To use:
+- Add tasks here or import them in the scheduler setup.
+- Each task must be async-compatible and independently callable.
+"""
