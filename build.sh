1fc66009782a6db5b1dc3b1aed3701a0225783d9
Vickonweb3 <vnnamdi534@gmail.com>
2025-08-11 18:49:34 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
 pip install -r requirements.txt
-
-# Skip system dependencies - use pre-downloaded binaries
-PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1 pip install playwright
-wget https://playwright.azureedge.net/builds/chromium/1105/chromium-linux.zip -O /tmp/chromium.zip
-unzip /tmp/chromium.zip -d /tmp/ms-playwright/chromium-1105
